31bb1812b002f45888c5627fefcdb6d629bcfc2f
nischal08 <nischal.karanjit@gmail.com>
2023-03-11 12:19:01 +0545

    fix: refined home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -49,164 +49,183 @@ class HomeScreen extends StatelessWidget {
       body: ScrollConfiguration(
         behavior: MyBehaviour(),
         child: SingleChildScrollView(
-          child: Column(
+          child: Stack(
             children: [
-              Stack(
+              Column(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                          height: 300,
-                          width: double.infinity,
-                          child: Image.asset(
-                            appbarBgImage,
-                            fit: BoxFit.cover,
-                          )),
-                      Container(
-                        height: 1000,
-                        color: AppColors.backgroundColor,
-                      )
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: AppSizes.paddingLg * 3,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: AppSizes.paddingLg),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            EachAppbarButton(iconData: Icons.menu_outlined),
-                            EachAppbarButton(
-                                iconData: Icons.shopping_bag_outlined),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: AppSizes.paddingLg * 2,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: AppSizes.paddingLg),
-                        width: .30.sh,
-                        child: Text(
-                          "Find the best cloths for you",
-                          style: bigTitleText.copyWith(
-                              height: 0.95,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 26.sp),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: AppSizes.paddingLg * 1.2,
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(left: AppSizes.paddingLg),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                              children: categoryList
-                                  .map((e) => EachCategoryItem(label: e))
-                                  .toList()),
-                        ),
-                      ),
-                       const SizedBox(
-                        height: AppSizes.paddingLg * 1.2,
-                      ),
-                      ScrollConfiguration(
-                        behavior: MyBehaviour(),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(32.r),
-                            color: AppColors.backgroundColor,
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: AppSizes.paddingLg,
-                          ),
-                          child: GridView.builder(
-                            physics: const ClampingScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: images.length,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              childAspectRatio: 0.62,
-                              crossAxisCount: 2,
-                              crossAxisSpacing: AppSizes.paddingLg,
-                            ),
-                            itemBuilder: (BuildContext context, int index) {
-                              return Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  Container(
-                                      height: 220,
-                                      clipBehavior: Clip.hardEdge,
-                                      decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(12.r),
-                                      ),
-                                      child: Image.network(
-                                        images[index],
-                                        fit: BoxFit.cover,
-                                      )),
-                                  const SizedBox(
-                                    height: AppSizes.padding,
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "Winter Jacket",
-                                            style: bodyText.copyWith(
-                                                fontWeight: FontWeight.w500),
-                                          ),
-                                          const SizedBox(
-                                            height: AppSizes.padding / 1.7,
-                                          ),
-                                          Text(
-                                            "\$129.00",
-                                            style: subTitleText.copyWith(
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ],
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {
-                                          //TODO: add to cart logic
-                                        },
-                                        child: Container(
-                                          padding: const EdgeInsets.all(
-                                              AppSizes.padding),
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(50.r),
-                                            color: AppColors.iconBtnBgColor,
-                                          ),
-                                          child: const Icon(
-                                            Icons.shopping_bag_outlined,
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              );
-                            },
-                          ),
-                        ),
-                      )
-                    ],
+                  SizedBox(
+                      height: 300,
+                      width: double.infinity,
+                      child: Image.asset(
+                        appbarBgImage,
+                        fit: BoxFit.cover,
+                      )),
+                  Container(
+                    height: 1000,
+                    color: AppColors.backgroundColor,
                   )
                 ],
               ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: AppSizes.paddingLg * 3,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: AppSizes.paddingLg),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Center(
+                              child: Text(
+                                "Welcome".toUpperCase(),
+                                style: smallText.copyWith(
+                                  color: AppColors.primaryColor,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.3,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 4.w),
+                            Text(
+                              "Niraj",
+                              style: bodyText.copyWith(
+                                color: AppColors.textDarkColor,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                        // EachAppbarButton(iconData: Icons.menu_outlined),
+                        const EachAppbarButton(
+                            iconData: Icons.shopping_bag_outlined),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: AppSizes.paddingLg * 2,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: AppSizes.paddingLg),
+                    width: .30.sh,
+                    child: Text(
+                      "Find the best cloths for you",
+                      style: bigTitleText.copyWith(
+                          height: 0.95,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26.sp),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: AppSizes.paddingLg * 1.2,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: AppSizes.paddingLg),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                          children: categoryList
+                              .map((e) => EachCategoryItem(label: e))
+                              .toList()),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: AppSizes.paddingLg * 1.2,
+                  ),
+                  ScrollConfiguration(
+                    behavior: MyBehaviour(),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(32.r),
+                        color: AppColors.backgroundColor,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: AppSizes.paddingLg,
+                      ),
+                      child: GridView.builder(
+                        physics: const ClampingScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: images.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          childAspectRatio: 0.62,
+                          crossAxisCount: 2,
+                          crossAxisSpacing: AppSizes.paddingLg,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Container(
+                                  height: 220,
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.circular(12.r),
+                                  ),
+                                  child: Image.network(
+                                    images[index],
+                                    fit: BoxFit.cover,
+                                  )),
+                              const SizedBox(
+                                height: AppSizes.padding,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Winter Jacket",
+                                        style: bodyText.copyWith(
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      const SizedBox(
+                                        height: AppSizes.padding / 1.7,
+                                      ),
+                                      Text(
+                                        "\$129.00",
+                                        style: subTitleText.copyWith(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      //TODO: add to cart logic
+                                    },
+                                    child: Container(
+                                      padding: const EdgeInsets.all(
+                                          AppSizes.padding),
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(50.r),
+                                        color: AppColors.iconBtnBgColor,
+                                      ),
+                                      child: const Icon(
+                                        Icons.shopping_bag_outlined,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
